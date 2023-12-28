14ad0ca1c21894cdc7f00671a57429d26b545d83
rayanabonfanti <rayanabonfanti@gmail.com>
2023-12-28 19:59:32 -0300

    fix: update pom.xml

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Desafio Serasa Experian
 
 Em desenvolvimento do texto...
+mvn clean install -Dprofile=test -Dmy-secret-key=my-secret-key -Dpassword-admin=admin
--- src/test/java/com/desafio/serasa/experian/FraudSystemApplicationTests.java
+++ src/test/java/com/desafio/serasa/experian/FraudSystemApplicationTests.java
@@ -1,16 +1,16 @@
-//package com.desafio.serasa.experian;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest(classes = FraudSystemApplication.class)
-//class FraudSystemApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//		FraudSystemApplication.main(new String[]{});
-//		Assertions.assertTrue(true);
-//	}
-//
-//}
+package com.desafio.serasa.experian;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FraudSystemApplication.class)
+class FraudSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+		FraudSystemApplication.main(new String[]{});
+		Assertions.assertTrue(true);
+	}
+
+}
